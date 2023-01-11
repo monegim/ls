a804766e0f6ac3c924ba71aa2c3ab3c765329340
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-11 22:22:09 +0330

    add different colors

--- cmd/ls/root.go
+++ cmd/ls/root.go
@@ -8,7 +8,13 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
+const (
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
 var version = "0.0.1"
 var rootCmd = &cobra.Command{
 	Use:     "ls",
