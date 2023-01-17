2d9584443ebfdfe745bc2ed7a16b35d684de7052
Mostafa Negim <mostafa.negim@gmail.com>
2023-01-17 23:58:10 +0330

    add TestGetOwnerShip

--- pkg/ls/utils_test.go
+++ pkg/ls/utils_test.go
@@ -1,7 +1,30 @@
 package ls
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestgetNames(t *testing.T) {
-	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetNames(t *testing.T) {
+	// path := "ls.go"
+	// // info, err := os.Stat(path)
+	// f, err := os.Open(path)
+	// assert.NoError(t, err)
+	// defer f.Close()
+	// file, err := f.ReadDir(1)
+	// info := file[0].Info()
+	// assert.NoError(t, err)
+	// stat := info.Sys().(*syscall.Stat_t)
+	// _ = stat
+}
+
+func TestGetOwnerShip(t *testing.T) {
+	path := "ls.go"
+	info, err := os.Stat(path)
+	assert.NoError(t, err)
+	uid, gid := getOwnership(info)
+	t.Log(uid)
+	t.Log(gid)
 }
